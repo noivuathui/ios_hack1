0aca662d658b79aedeb5e6e3aca72f2b92fe449b
Nguyen Duc Tai <noivuathoiyou@gmail.com>
2016-05-08 10:16:10 +0700

    up2

--- Dictionary/Tripsmore/ViewControllers/FavouriteViewController.m
+++ Dictionary/Tripsmore/ViewControllers/FavouriteViewController.m
@@ -57,7 +57,7 @@
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section;
 {
-    return 10;
+    return 5;
 }
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
